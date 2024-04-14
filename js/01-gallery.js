4134ac7d007b738b53fe225b74b1c0eac73e263d
CristinaIlicea <ilicea.cristina@yahoo.com>
2024-04-15 00:33:03 +0300

    ex 1 draft

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const array = `${galleryItems}`;
+for (let index = 0; index < array.length; ++index) {
+    const element = array[index];
+    const instance = basicLightbox.create(`
+    <li class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+        <img
+          class="gallery__image"
+          src="small-image.jpg"
+          data-source="large-image.jpg"
+          alt="./gallery-items.js/description"
+        />
+      </a>
+    </li>
+    `)
+    instance.show()
+};
 
-console.log(galleryItems);
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <img>
+            src = "./gallery-items.js/original"
+        </img>
+    </div>
+`)
+
+instance.show()
