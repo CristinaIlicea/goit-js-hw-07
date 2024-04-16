ed1df143863cbb6273dce82de140909d69ef7bae
CristinaIlicea <ilicea.cristina@yahoo.com>
2024-04-16 12:30:03 +0300

    tema 1 final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,43 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-function createImages(image) {
-    return image
-      .map(
-        ({ preview, original, description }) =>
-          //marcajul elementelor din galerie (sablon)
-          `<li class="gallery__item">
-              <a class="gallery__link" target="_blank" href="${original}">
-                  <img
-                      class="gallery__image"
-                      src="${preview}"
-                      data-source="${original}"
-                      alt="${description}"
-                  />
-              </a>
-          </li>`
-      )
-      .join("");
-  }
-  let image = document.querySelectorAll(".gallery__item")
-  image[0].insertAdjacentHTML("beforeend", createImages);
-  image.addEventListener("click", openModalPhotoOnClick);
-  function openModalPhotoOnClick(e) {
-    e.preventDefault();
-    const currentImage = e.target.dataset.source;
-    const instance = basicLightbox.create(
-      `
-      <img src="${currentImage}" width="800" height="600">
-  `,
-      {
-        onShow: () => {
-          window.addEventListener("keydown", closeOnEscClick);
-        },
-        onClose: () => {
-          window.removeEventListener("keydown", closeOnEscClick);
-        },
-      }
-    );
-    if (e.target !== e.currentTarget) instance.show();
-    function closeOnEscClick(e) {
-      if (e.code === "Escape") instance.close();
-    }};
+
+  const gallery = document.querySelector('.gallery');
+  // creare galerie HTML
+  galleryItems.forEach(item => {
+    const li = document.createElement('li');
+    li.classList.add("style-img");
+    const a = document.createElement('a');
+    a.classList.add("gallery__link");
+    a.setAttribute("href", item.original);
+    const img = document.createElement('img');
+    img.src = item.preview;
+    img.alt = item.description;
+     img.setAttribute('data-source', item.original);
+    li.appendChild(a);
+    a.appendChild(img);
+    gallery.appendChild(li);
+  });
+  // stop download
+  const img = document.querySelectorAll('img');
+  img.forEach((image) => {
+      image.addEventListener('click', function(event) {
+          event.preventDefault();
+      });
+  });
+      // library basicLightbox
+  document.querySelectorAll('.gallery img').forEach(img => {
+    img.addEventListener('click', function () {
+     const imageIndex = galleryItems.findIndex(item => item.preview === img.src);
+      const instance = basicLightbox.create(`<img src=${galleryItems[imageIndex].original} width="800" height="600">`);
+      instance.show();
+    });
+  }); 
